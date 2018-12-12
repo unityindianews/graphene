cf36ccdf0bc636c22ab121e669a6084e486e0ec2
Raja <raja@ghcbp.com>
2018-12-12 16:23:28 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  moduleId: module.id.replace("/dist/", "/src/")
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { RouterModule } from '@angular/router';
+import { Approute } from './app.routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferenceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(Approute,{useHash:true})
   ],
-  providers: [],
+  providers: [ReferenceComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
