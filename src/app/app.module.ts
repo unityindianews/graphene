6114555976a4d01aed5db95b9197d681501ef396
rajadhanesh <36322478+rajadhanesh@users.noreply.github.com>
2018-12-17 20:18:52 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -13,17 +13,20 @@ import { AnalysisComponent } from './reference/analysis/analysis.component';
 import { DefaultPipe } from './_pipe/default.pipe';
 import { CustomDirective } from './_directives/custom.directive';
 import { AnalysisService } from './_service/analysis.service';
+import { ResearchComponent } from './reference/research/research.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomepageComponent,
     CrudComponent,
     FormValidationComponent,
     HeaderComponent,
-    HomepageComponent,
     AnalysisComponent,
+    ResearchComponent,
     DefaultPipe,
     CustomDirective
   ],
